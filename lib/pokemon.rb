012555fa3c4868c01be02100602817ee97b0b8c8
derek <derekle.creative@gmail.com>
2021-04-15 23:27:00 -0500

    commit tree 80e1df22f4aeb4b23cc05ff4b46ae280388ef92e

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+        p self
+    end
+    
+    def self.new_from_db(array)
+        p array[0]
+        p array[1]
+        p array[2]
+        p array[3]
+
+
+        pokemon = self.new(id: array[0], name: array[1], type: array[2], db: array[3])
+      end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id = ?
+        LIMIT 1
+      SQL
+  
+      db.execute(sql, id).map do |row|
+        self.new_from_db(row)
+      end[0]
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+    end
 end
